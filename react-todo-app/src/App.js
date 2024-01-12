bc3e4bf8b601027459ceae68c753e2c1d98c2b25
sofara <sdi2000136@di.uoa.gr>
2024-01-12 18:15:50 +0200

    1

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,15 +1,38 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
+import headerImage from './header-image.png';
 import './App.css';
 
+console.log(logo);
+console.log(headerImage)
+
+
 function App() {
+  
   return (
-    <div className="App">
-       <h1> React Todo App</h1>
-       <div className="input-wrapper">
-        <input type="text" name="todo" placeholder="Create a new Todo" />
-        <button className="add-button">Add</button>
-       </div>
+    <header>
+    <div id="header">
+      <div id="logo"> 
+        <img src ={logo} alt="Logo" />
+      </div>
+      
+    <nav id="menu">
+      <ul>
+        <li class="active">
+          <a href='#'> Συνδεση</a>
+        </li>
+        <li>
+          <a href='#'> Βοηθεια</a>
+        </li>
+      </ul>
+    </nav>
     </div>
+    <div id="header-image-menu">
+      <img src={headerImage} alt="Image of the National and Kapodistrian University of Athens" />
+      <h2 id="image-text">
+           Εθνικό και Καποδιστριακό Πανεπιστήμιο Αθηνών
+      </h2>
+    </div>
+  </header>
   );
 }
 
