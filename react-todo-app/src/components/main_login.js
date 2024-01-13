091286793c7999ec9b76ffb66f1f7dc1e5b87edc
sofara <sdi2000136@di.uoa.gr>
2024-01-13 22:03:30 +0200

    1

--- react-todo-app/src/components/main_login.js
+++ react-todo-app/src/components/main_login.js
@@ -2,34 +2,36 @@ import React from "react";
 import "./../css/main_login.css";
 
 export default function Login() {
-    return (
-        <div className="user-groupslogin">
-        <div className="div1">
-          <ul className="ul">
-            <li className="li">Παρακολουθήστε/ δημιουργήστε βαθμολόγια</li>
-            <li>Βρείτε πληροφορίες για τα μαθήματα που διδάσκετε</li>
-          </ul>
-        </div>
-        <div className="div2">
-          <ul className="ul">
-            <li className="li">Βρείτε πληροφορίες για τα μαθήματα</li>
-            <li className="li">Κάνετε δήλωση μαθημάτων</li>
-            <li className="li">Εκτυπώστε τη βαθμολογία σας</li>
-            <li>Κάνετε αίτηση για την έκδοση πιστοποιητικών</li>
-          </ul>
-        </div>
-        <b className="b1">Διδάσκοντες</b>
-        <b className="b2">Φοιτητές</b>
+  return (
+    <div className="user-groupslogin">
+      <div className="div1">
+        <ul className="ul">
+          <li className="li">Παρακολουθήστε/ δημιουργήστε βαθμολόγια</li>
+          <li>Βρείτε πληροφορίες για τα μαθήματα που διδάσκετε</li>
+        </ul>
+      </div>
+      <div className="div2">
+        <ul className="ul">
+          <li className="li">Βρείτε πληροφορίες για τα μαθήματα</li>
+          <li className="li">Κάνετε δήλωση μαθημάτων</li>
+          <li className="li">Εκτυπώστε τη βαθμολογία σας</li>
+          <li>Κάνετε αίτηση για την έκδοση πιστοποιητικών</li>
+        </ul>
+      </div>
+      <b className="b1">Διδάσκοντες</b>
+      <b className="b2">Φοιτητές</b>
+      <div class="user-group-images">
         <img
-          className="image-placeholder2-icon"
-          alt=""
+          className="user-group-icon"
+          alt="Teacher"
           src="/teacher.png"
         />
         <img
-          className="image-placeholder1-icon"
-          alt=""
-          src="/students.png"
+          className="user-group-icon"
+          alt="Student"
+          src="./students-image.png"
         />
       </div>
-    );
+    </div>
+  );
 }
