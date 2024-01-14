14259838b77451c539c21fa946a1daf36645809b
sofara <sdi2000136@di.uoa.gr>
2024-01-14 19:52:47 +0200

    1

--- react-todo-app/src/components/Nav1.js
+++ react-todo-app/src/components/Nav1.js
@@ -3,14 +3,17 @@ import {Link} from "react-router-dom";
 import "./../css/HomePage.css";
 import Button from "./../components/Button1.js";
 import SignInButton from "./../components/SignInButton.js";
+import HomePage from "../pages/HomePage.js";
 
 export default function Nav1() {
     return (
         <header class="sticky">
             <div id="header">
+               
                 <div id="logo">
-                    <img src="logo.png" alt="Logo" />
-                </div>
+                <Link to="/">
+                    <img src="logo.png" alt="Logo" /></Link>
+                </div> 
                 <nav id="menu">
                     
                     <ul>
