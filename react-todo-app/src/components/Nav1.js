a543c05ae2ee6b822459af8d4826447544c289a4
sofara <sdi2000136@di.uoa.gr>
2024-01-14 08:26:40 +0200

    homepage change

--- react-todo-app/src/components/Nav1.js
+++ react-todo-app/src/components/Nav1.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./../css/App.css";
 import Button from "./../components/Button1.js";
+import SignInButton from "./../components/SignInButton.js";
 
 export default function Nav1() {
     return (
-        <header>
+        <header class="sticky">
             <div id="header">
                 <div id="logo">
                     <img src="logo.png" alt="Logo" />
                 </div>
                 <nav id="menu">
+                    
                     <ul>
                         <li> <a href='#'>   <Button /></a>
+                        <li> <a href='#'> <SignInButton /> </a> </li>
+
                          
                         </li>
                     </ul>
--- react-todo-app/src/components/main_login.js
+++ react-todo-app/src/components/main_login.js
@@ -1,37 +1,33 @@
 import React from "react";
 import "./../css/main_login.css";
+import SignInButton from "./SignInButton";
+
+/**Προσπαθησα να φτιαξω το text διπλα απο την εικονα αλλα δεν μπορουσα με τιποτα να τα βαλω το ενα κατω απο το αλλο και
+ * κεντραρισμενα με τιποτα και εκανα rage quit. Αν μπορεις δεν αν ειναι αν το φτιαξεις. Ολσο, εκανα το nav bar sticky για να 
+ * μενει πανω πανω οταν κανεις scroll. */
+/**εβαλα το κουμπι συνδεση γτ δεν μου φαινοταν πολυ εμφανες το οτι πρεπει αν πατησουν στο "ειμαι φοιτητης" αλλα μπορουμε να κανουμε
+ * αυτο πιο πολυ σαν κουμπι και να το βγαλουμε αυτο
+ */
 
 export default function Login() {
   return (
-    <div className="user-groupslogin">
-      <div className="div1">
-        <ul className="ul">
-          <li className="li">Παρακολουθήστε/ δημιουργήστε βαθμολόγια</li>
-          <li>Βρείτε πληροφορίες για τα μαθήματα που διδάσκετε</li>
+    <div class="user-groupslogin">
+      <div id="user-groups" >
+        <ul>
+          <li><a href='#'> <img class="user-group-icon" src="student-icon.png" ></img> </a> </li>
+
+          <li> <div class="box1"> <ul> <li> <a href='#'> <p class="text-main">Είμαι Φοιτητής</p></a> </li>
+            <li><span id="text-main2">Βρείτε πληροφορίες για τα μαθήματα σας.<br></br>
+              Κάντε δήλωση μαθημάτων. <br></br>
+              Δείτε και εκτυπώστε τη βαθμολογία σας. <br></br>
+              Κάντε αίτηση για την έκδοση πιστοποιητικών.</span></li>
+            <li> <a href='#'> <SignInButton /> </a></li></ul>
+
+          </div>
+          </li>
         </ul>
       </div>
-      <div className="div2">
-        <ul className="ul">
-          <li className="li">Βρείτε πληροφορίες για τα μαθήματα</li>
-          <li className="li">Κάνετε δήλωση μαθημάτων</li>
-          <li className="li">Εκτυπώστε τη βαθμολογία σας</li>
-          <li>Κάνετε αίτηση για την έκδοση πιστοποιητικών</li>
-        </ul>
-      </div>
-      <b className="b1">Διδάσκοντες</b>
-      <b className="b2">Φοιτητές</b>
-      <div class="user-group-images">
-        <img
-          className="user-group-icon"
-          alt="Teacher"
-          src="/teacher.png"
-        />
-        <img
-          className="user-group-icon"
-          alt="Student"
-          src="./students-image.png"
-        />
-      </div>
-    </div>
+    </div >
   );
 }
+
