3f780997f64cd9d7ead6c3b1610ac5e62c52cc45
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-01-15 20:13:48 +0200

    new

--- react-todo-app/src/pages/Teachers.js
+++ react-todo-app/src/pages/Teachers.js
@@ -15,10 +15,30 @@ export default function Teachers() {
       </div>
       <div className="teachers_body">
             <div className="teacher_box">
-            <div className="teacher_div1">ΒΑΘΜΟΛΟΓΙΑ</div>
-            <div className="teacher_div2">ΜΑΘΗΜΑΤΑ</div>
-            <div className="teacher_div3">ΜΑΘΗΜΑΤΑ</div>
-            <div className="teacher_div4">ΜΑΘΗΜΑΤΑ</div>
+            <div className="teacher_div1">
+                <img className="timage" alt="" src="/bathmologia.png" />
+                <div className="teachers_div">Τα Βαθμολόγια Μου
+                    <div className="teachers_div1">Δείτε βαθμολόγια που έχετε <br></br>δημιουργήσει στο παρελθόν</div>
+                </div>
+            </div>
+            <div className="teacher_div2">
+                <img className="timage" alt="" src="/mathima.png" />
+                <div className="teachers_div">Πληροφορίες Μαθημάτων
+                    <div className="teachers_div1">Δείτε πληροφορίες για τα <br></br>μαθήματα που διδάσκετε</div>
+                </div>
+            </div>
+            <div className="teacher_div3">
+                <img className="timage" alt="" src="/bathmologia.png" />
+                <div className="teachers_div">Δημιουργία Νέου Βαθμολογίου Μεμονωμένα
+                    <div className="teachers_div1">Δημιουργήστε νέο βαθμολόγιο με <br></br>μεμονωμένη εισαγωγή βαθμών</div>
+                </div>
+            </div>
+            <div className="teacher_div4">
+                <img className="timage" alt="" src="/bathmologia.png" />
+                <div className="teachers_div">Δημιουργία Νέου Βαθμολογίου Μαζικά
+                    <div className="teachers_div1">Δημιουργήστε νέο βαθμολόγιο με <br></br>ανέβασμα αρχείου</div>
+                </div>
+            </div>
             </div>
         </div>
       <Footer />
