88ee641ce162e50d0f3b37cfa07f339d887a103c
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-01-17 22:24:19 +0200

    r

--- react-todo-app/src/pages/Student_dilosi.js
+++ react-todo-app/src/pages/Student_dilosi.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from 'react';
 import { db } from '../components/firebase.js';
-import { doc, setDoc, updateDoc, arrayUnion } from 'firebase/firestore'
+import { doc, setDoc, updateDoc, arrayUnion, getDoc, getDocs, collection } from 'firebase/firestore'
 import {Link} from "react-router-dom";
 import "./../css/HomePage.css";
 import "./../css/Student_dilosi.css";
@@ -10,13 +10,25 @@ import Footer from "./../components/footer.js"
 import Menu from "./../components/student_menu.js"
 
 export default function Student_dilosi() {
+    const [courses, setCourses] = useState();
+    async function getAllCoursesFunc (e){
+        e.preventDefault()
+        
+        //"Bring me, from the collection 'courses' the document with name/value 'all_courses'"
+        const ref = doc(db, "courses", "all_courses"); 
+        const res = await getDoc(ref);
 
-    //const ref = doc(db, "courses", "all_courses");
-    //const res = getDoc(ref);
-
-    //const user_role = res.data().name
-
-
+        if (res.exists()) {
+            // This part just make your response look more like a JSON and less like a text
+            var ugly = JSON.stringify(res.data());
+            var obj = JSON.parse(ugly);
+            var pretty = JSON.stringify(obj, undefined, 4);
+            // Save response to a string state
+            setCourses(pretty)
+        } else {
+            console.log("No such document!");
+        }
+    }
     useEffect(()=> {
         // Every time you try to enter this page check if you have a saved key at the local storage. 
         // If not, then do not allow user to enter this page and redirect to login page
@@ -29,8 +41,18 @@ export default function Student_dilosi() {
         <div>
             <Nav2 />
             <Menu />
-            <div className='courses'>
-                {/* <textarea rows={5} cols={50} style={{'margin': '10px'}} value={courses}/ >    */}
+            <div>
+                {getAllCoursesFunc}
+                <textarea rows={5} cols={50} style={{'margin': '10px'}} value={courses}/ >
+                <table>
+                    <thead>
+                        <tr>id</tr>
+                        <tr>name</tr>
+                    </thead>
+                    <tbody>
+                        <tr>{courses}</tr>
+                    </tbody>
+                </table>
             </div>
             <Footer />
         </div>
