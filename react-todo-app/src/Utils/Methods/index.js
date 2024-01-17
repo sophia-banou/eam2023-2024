4d53ec58cea0a7b2290912ccbe4a06a17643ae90
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-01-17 19:25:45 +0200

    k

--- react-todo-app/src/Utils/Methods/index.js
+++ react-todo-app/src/Utils/Methods/index.js
@@ -1,9 +1,11 @@
 // Logout checks if you have the specific keys stored in your local storage. 
 // If you have then deletes them and redirects to login page.
 export function logout () {
-    if (localStorage.getItem('role') !== null || localStorage.getItem('email') !== null) {
+    if (localStorage.getItem('role') !== null || localStorage.getItem('email') !== null || localStorage.getItem('AM') !== null || localStorage.getItem('name') !== null) {
         localStorage.removeItem('role')
-        localStorage.getItem('email')
+        localStorage.removeItem('name')
+        localStorage.removeItem('AM')
+        localStorage.removeItem('email')
         window.location.href = '/'
     }
 }
