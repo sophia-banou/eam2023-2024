ca5f76c81f7c076cfe569580954281fd6f29b786
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-01-18 20:02:17 +0200

    y

--- react-todo-app/src/pages/Student_dilosi.js
+++ react-todo-app/src/pages/Student_dilosi.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { db } from '../components/firebase.js';
 import {Link} from "react-router-dom";
 import "./../css/HomePage.css";
@@ -7,9 +7,19 @@ import "./../css/Student_dilosi.css";
 import Nav2 from "./../components/Nav2.js"
 import Footer from "./../components/footer.js"
 import Menu from "./../components/student_menu.js"
+import { Button2 } from "../components/Button1.js";
 
 export default function Student_dilosi() {
-
+    const [checked, setChecked] = useState([]);
+    const handleCheck = (event) => {
+        var updatedList = [...checked];
+        if (event.target.checked) {
+          updatedList = [...checked, event.target.value];
+        } else {
+          updatedList.splice(checked.indexOf(event.target.value), 1);
+        }
+        setChecked(updatedList);
+      };
     useEffect(()=> {
         // Every time you try to enter this page check if you have a saved key at the local storage. 
         // If not, then do not allow user to enter this page and redirect to login page
@@ -33,28 +43,28 @@ export default function Student_dilosi() {
                             <th class="dcell">Κατηγορία</th>
                         </tr>
                         <tr>
-                            <th ><input type="checkbox" /></th>
+                            <th ><input value={1} type="checkbox" onChange={handleCheck} /></th>
                             <th>Μάθημα</th>
                             <th>Α</th>
                             <th>8ο</th>
                             <th>Ελεύθερο</th>
                         </tr>
                         <tr class="drow2">
-                            <th><input type="checkbox" /></th>
+                            <th><input value={1} type="checkbox" onChange={handleCheck} /></th>
                             <th>Μάθημα</th>
                             <th>Α</th>
                             <th>8ο</th>
                             <th>Ελεύθερο</th>
                         </tr>
                         <tr>
-                            <th><input type="checkbox" /></th>
+                            <th><input value={1} type="checkbox" onChange={handleCheck} /></th>
                             <th>Μάθημα</th>
                             <th>Α</th>
                             <th>8ο</th>
                             <th>Ελεύθερο</th>
                         </tr>
                         <tr class="drow2">
-                            <th><input type="checkbox" /></th>
+                            <th><input value={1} type="checkbox" onChange={handleCheck} /></th>
                             <th>Μάθημα</th>
                             <th>Α</th>
                             <th>8ο</th>
@@ -62,8 +72,14 @@ export default function Student_dilosi() {
                         </tr>
                     </table>
                 </div>
+                <Link to="/aitisi_teachers">
+                <div className="dilosi_rectangle1">
+                    <div className="dilosi_div">Προσωρινή Αποθήκευση</div>
+                </div>
+                </Link>
             </div>
+            
             <Footer />
         </div>
     );
-};
+}
