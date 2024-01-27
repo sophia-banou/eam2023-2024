efaf1b5dc72a91c27e1777069a27e0e1cceb2e82
sofara <sdi2000136@di.uoa.gr>
2024-01-27 22:18:24 +0200

    fix home

--- react-todo-app/src/pages/HomePage.js
+++ react-todo-app/src/pages/HomePage.js
@@ -16,35 +16,41 @@ export default function HomePage() {
             <h2 class="image-text"> ΓΡΑΜΜΑΤΕΙΕΣ ΠΑΝΕΠΙΣΤΗΜΙΟΥ ΑΘΗΝΩΝ </h2>
             <span class="image-text2">Εισάγετε τα στοιχεία σας για να αποκτήσετε <br></br> πρόσβαση στις υπηρεσίες της πλατφόρμας.</span>
           </div>
-          <Link to='/login'><div class="button-div1"><button class="header-button"> ΣΥΝΔΕΣΗ</button></div></Link>
         </div>
       </div>
       <div class="info-div1">
         <div class="info-div3">
 
           <div class="info-box1">
-            <div class="info-img-div">
-              <img src="st-icon.png" class="info-box-img"></img>
-            </div>
-            <div class="info-b-text">
+            <a class="link" href="/login">
+              <div class="info-img-div">
+                <img src="st-icon.png" class="info-box-img"></img>
+              </div>
+              <div class="info-b-text">
                 <div>
-                    <span class="info-span1"> Φοιτητές</span> <br></br>
-                    <span class="info-span2"> Δείτε τις βαθμολογίες σας, δείτε τις προθεσμίες των δηλώσεων και δηλώστε μαθήματα, δημιουργήστε αιτήσεις για πιστοποιητικά</span>
+                  <span class="info-span1"> Φοιτητές</span> <br></br>
+                  <span class="info-span2"> Δείτε τις βαθμολογίες σας, δείτε τις προθεσμίες των δηλώσεων και δηλώστε μαθήματα, δημιουργήστε αιτήσεις για πιστοποιητικά</span>
                 </div>
-            </div>
+              </div>
+            </a>
           </div>
+
           <div class="info-box1">
-            <div class="info-img-div">
-              <img src="pr-icon.png" class="info-box-img"></img>
-            </div>
-            <div class="info-b-text">
+            <a class="link" href="/login">
+              <div class="info-img-div">
+                <img src="pr-icon.png" class="info-box-img"></img>
+              </div>
+              <div class="info-b-text">
                 <div>
-                    <span class="info-span1"> Διδάσκοντες</span> <br></br>
-                    <span class="info-span2"> Δείτε τις βαθμολόγια σας και δημιουργήστε καινούργια, δείτε πληροφορίες για τα μαθήματα που διδάσκετε</span>
+                  <span class="info-span1"> Διδάσκοντες</span> <br></br>
+                  <span class="info-span2"> Δείτε τις βαθμολόγια σας και δημιουργήστε καινούργια, δείτε πληροφορίες για τα μαθήματα που διδάσκετε</span>
                 </div>
-            </div>  
-           
+
+              </div>
+
+            </a>
           </div>
+
 
         </div>
 
